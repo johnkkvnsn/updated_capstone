01170d412f28f879e54dd568cf2e0b8ed89d1f68
johnkkvnsn <0323-3554@lspu.edu.ph>
2026-07-28 11:36:46 +0800

    Deployment preparation: Added Dockerfile and Environment Variables

--- api/config.php
+++ api/config.php
@@ -10,10 +10,10 @@ session_set_cookie_params([
 ]);
 session_start();
 
-$host = 'localhost';
-$db   = 'bfmss_db';
-$user = 'root';
-$pass = '';
+$host = getenv('DB_HOST') ?: 'localhost';
+$db   = getenv('DB_NAME') ?: 'bfmss_db';
+$user = getenv('DB_USER') ?: 'root';
+$pass = getenv('DB_PASS') ?: '';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
